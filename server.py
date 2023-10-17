b1e00ec9bd0bf28d1a0c9317893a65e495273b3f
“Danielle <ramirezdanielle88@gmail.com>
2023-10-16 18:16:26 -0700

    second commit

--- server.py
+++ server.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template ('index.html')
 
-@app.route('/bio')
-def about():
-    return render_template ('about.html')
+# @app.route('/')
+# def about():
+#     return render_template ('index.html')
+
+# @app.route('/')
+# def games():
+#     return render_template ('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
